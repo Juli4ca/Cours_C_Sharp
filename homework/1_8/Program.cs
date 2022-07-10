5a4ef997d3665f755598b76f32e4d6090426e002
Yuliya <sirena_travel@mail.ru>
2022-07-10 20:36:16 +0300

    Task 1_8 finish formatting

--- homework/1_8/Program.cs
+++ homework/1_8/Program.cs
@@ -5,15 +5,26 @@ int i = 2;
 
 while (i <= number)
 {
-    if (i != number)
+    if (i != number && number % 2 == 0)
     {
         Console.Write(i);
         Console.Write(", ");
         i += 2;
     }
+    else if (i == number && number % 2 == 0)
+    {
+        Console.Write(i);
+        i += 2;
+    }
+    else if (number % 2 != 0 && i == number - 1)
+    {
+        Console.Write(i);
+        i += 2;
+    }
     else
     {
         Console.Write(i);
+        Console.Write(", ");
         i += 2;
     }
  
