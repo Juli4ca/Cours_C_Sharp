d45387b8a8ca15baa5336591a3ee1046aaa63bfa
Yuliya <sirena_travel@mail.ru>
2022-07-12 12:45:08 +0300

    homework Task 2_1

---
+++ homework/2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+Console.Write("Write a three-digit number N: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n > 999 ^ n < 100)
+{
+    Console.Write("Invalid value N");
+}
+else
+{
+    Console.WriteLine(n % 100 / 10);
+}
