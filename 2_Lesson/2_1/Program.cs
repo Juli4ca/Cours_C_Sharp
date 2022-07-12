a2d42f57f939e20e7bcd545d65055a3cbbbdc0e4
Yuliya <sirena_travel@mail.ru>
2022-07-12 12:38:17 +0300

    Task 2_1

--- 2_Lesson/2_1/Program.cs
+++ 2_Lesson/2_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая принимает
+// на вход трёхзначное число и на выходе
+// показывает последнюю цифру этого числа.
+
+Console.Write("Write a three-digit number N: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n > 999 ^ n < 100)
+{
+    Console.Write("Invalid value N");
+}
+else
+{
+    Console.WriteLine(n % 10);
+}
