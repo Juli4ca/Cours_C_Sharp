6a6667e8490c464a63b2d89b293ff75a0e532112
Yuliya <sirena_travel@mail.ru>
2022-07-12 18:16:51 +0300

    homework Task 2_4

--- homework/2_4/Program.cs
+++ homework/2_4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+Console.Write("Enter the number indicating the day of the week: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 1 || n > 7)
+{
+    Console.WriteLine("Invalid value");
+}
+else
+{
+    if (n < 6)
+    {
+        Console.WriteLine("It`s a weekday");
+    }
+    else
+    {
+        Console.WriteLine("It`s a weekend");
+    }
+}
