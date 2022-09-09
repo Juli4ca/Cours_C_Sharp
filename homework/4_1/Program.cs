6928055d820addd32d8175d501f1bd1056a628df
Yuliya <sirena_travel@mail.ru>
2022-09-09 21:47:25 +0300

    homework 4_1

--- homework/4_1/Program.cs
+++ homework/4_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+void exponent (int a, int b)
+{
+    int res = a;
+    for (int i = 1; i < b; i++)
+    {
+        res = res * a;
+    }
+    
+    Console.WriteLine(res);
+
+}
+
+exponent(2, 4);
+
