37703f0a8dd7596e243de48fcf2fc4dbdc19684f
Yuliya <sirena_travel@mail.ru>
2022-09-09 22:55:49 +0300

    homework 4_3

---
+++ homework/4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int sumDigits (string n)
+{
+    int res = int.Parse(n);
+    
+    for (int i = 1; i < n.Length; i++)
+    {
+        res = res + (res % 10);
+    }
+    return res;
+}
+
+
+Console.WriteLine(sumDigits("452"));
---
+++ homework/4_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void Randome01Array (int num)
+{
+    int [] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next();
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Randome01Array(8);
