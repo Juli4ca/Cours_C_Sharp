14cb747c8ffdaf5d5855bc8d7d3bb9ae0a587b3b
Yuliya <sirena_travel@mail.ru>
2022-09-15 17:35:56 +0300

    task 6_1

---
+++ 6_Lesson/6_1/Program.cs
@@ -0,0 +1,41 @@
+﻿//Перевернуть массив
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($" {arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums (int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int[] InvertMass (int[] arr)
+{
+    int help = 0;
+    int size = arr.Length;
+    for (int i = 0; i < size / 2; i++)   
+    {
+        help = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = help;
+    }
+    
+    return(arr);
+}
+
+int[] arr_1 = MassNums(4, 0, 11);
+Print(arr_1);
+int[] new_arr_1 = InvertMass(arr_1);
+Print(new_arr_1);
