4ce7bbb741971aa7015a82dfbe2e52df9029350a
Yuliya <sirena_travel@mail.ru>
2022-09-16 15:01:05 +0300

    task 6_4

--- 6_Lesson/6_4/Program.cs
+++ 6_Lesson/6_4/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($" {arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums (int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int[] CopiMas(int[]arr)
+{
+    int size = arr.Length;
+    int[] new_arr = new int[size];
+    for (int i = 0; i < size; i++) new_arr[i] = arr[i];
+    return (new_arr);
+}
+
+int[]arr_1 = MassNums(10, 0, 101);
+Print(arr_1);
+int[] arr_copi = CopiMas(arr_1);
+Print(arr_copi);
