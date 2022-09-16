bf68c3e6c5fd1bffce2f4615b430367d666f2573
Yuliya <sirena_travel@mail.ru>
2022-09-16 18:34:13 +0300

    task 6_2 homework

---
+++ homework/6_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+void CoordinatesIntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = 0;
+    
+    if (k1 * x + b1 != k2 * x + b2) Console.WriteLine("У данных прямых нет точки пересечения");
+
+    else
+    {
+        y = k1 * x + b1;
+        Console.WriteLine($"Координаты точки пересечения данных прямых: ({x}; {y})");
+    }
+
+}
+
+CoordinatesIntersectionPoint(2, 5, 4, 9);
+CoordinatesIntersectionPoint(1,2,8,12);
